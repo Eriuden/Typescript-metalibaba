34dbd375ae8a00e5bc45d9131930d2ac98926a40
Eriuden <antoinecarrette@hotmail.fr>
2024-11-06 09:15:53 +0100

    build: error redux reducer

---
+++ Client/src/redux/reducers/errorsReducer.js
@@ -0,0 +1,19 @@
+import { GET_ARTICLE_ERROR } from "../actions/article.actions";
+import { GET_USER_ERRORS } from "../actions/user.actions";
+
+const initialState = {userError : [], articleError : []}
+
+export const errorReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ARTICLE_ERROR:
+            return {
+                articleError: action.payload
+            }
+        case GET_USER_ERRORS:
+            return {
+                userError: action.payload 
+            }    
+        default:
+            return state
+    }
+}
