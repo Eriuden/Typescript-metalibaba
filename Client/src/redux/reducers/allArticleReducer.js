6c2edcb6355d12cfe1e8849b4b7456aebc6de3fd
Eriuden <antoinecarrette@hotmail.fr>
2024-11-06 09:13:00 +0100

    build: users and articles redux reducer

---
+++ Client/src/redux/reducers/allArticleReducer.js
@@ -0,0 +1,12 @@
+import { GET_ALL_ARTICLES } from "../actions/article.actions";
+
+const initialState = {}
+
+export const allArticleReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case GET_ALL_ARTICLES:
+            return action.payload
+        default:
+            return state
+    }
+}
