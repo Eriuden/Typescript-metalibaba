4d9ae0b890c105dc7df1a18d72fcfb24be14c400
Eriuden <antoinecarrette@hotmail.fr>
2024-11-06 09:18:11 +0100

    build: cart redux reducer

---
+++ Client/src/redux/reducers/cartReducer.js
@@ -0,0 +1,40 @@
+import { MODIFY_CART, CLEAN_CART } from "../Actions/Cart.action";
+
+let localCart = JSON.parse(window.localStorage.getItem("metalibaba"))
+
+if (localCart === null) {
+    localCart = []
+}
+
+let totalPrice = calculateTotalPrice (localCart)
+
+const initialState = {
+    cart: localCart,
+    totalPrice: totalPrice
+}
+
+const calculateTotalPrice = (cart) => {
+    let priceTotal = 0
+
+    for (let i = 0; i < cart.length; i++) {
+        let total = parseFloat(cart[i].price) * parseInt(cart[i].quantityInCart)
+        priceTotal += total
+    }
+
+    return priceTotal
+}
+
+export const cartReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case MODIFY_CART:
+            let totalPrice = calculateTotalPrice(action.payload)
+
+            return { cart : action.payload, totalPrice: totalPrice}
+        
+        case CLEAN_CART:
+            return {cart: [], totalPrice: 0}
+        
+        default: 
+            return state
+    }
+}
