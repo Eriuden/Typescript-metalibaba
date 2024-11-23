e72592833ab266a5eb03f81cc3e7c778ad817f11
Eriuden <antoinecarrette@hotmail.fr>
2024-11-23 18:48:20 +0100

    build: comments

---
+++ Client/src/components/comments/Comments.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import {addCommentArticle, getArticle} from "../../redux/actions/article.actions"
+import {useDispatch, useSelector} from "react-redux"
+import EditDeleteComment from "./EditDeleteComment";
+import {isEmpty} from "../../Utils"
+
+
+
+export const Comments = (commentProps: any) => {
+  const [commentText, setCommentText] = useState("")
+  const user = useSelector((state: any) => state.userReducer)
+  const users = useSelector((state: any)=> state.allUsersReducer)
+  const dispatch = useDispatch
+
+  const handleComments = (e:any) => {
+      e.preventDefault()
+
+      if (commentText) {
+          dispatch(addCommentArticle(commentProps._id, user._id, commentText, user.name))
+              .then(()=> dispatch(getArticle()))
+              .then(()=> setCommentText(""))
+      }
+  }
+
+return (
+  <div>
+      {commentProps.comments.map((comment: any)=> {
+          return (
+              <div key={comment._id}>
+                  <div>
+                      <img src={!isEmpty(users[0]) && users.map((user: any)=> {
+                          if(user._id === comment.commenterId) return user.picture
+                          else return null
+                      })
+                      .join("")
+                      }
+                      />
+                  </div>
+
+                  <div>
+                      <h3>{comment.commenterName}</h3>
+                  </div>
+                  <p>{comment.text}</p>
+                  <EditDeleteComment props={comment} 
+                  articleId = {commentProps._id}/>
+              </div>
+          )
+      })}
+
+      {user._id && (
+          <form action='' onSubmit={handleComments}>
+              <input
+              type="text"
+              name='text'
+              onChange={(e)=> setCommentText(e.target.value)}
+              value={commentText}
+              placeholder="Laisser un commentaire"
+              />
+              <br/>
+              <input type="submit" value="Envoyer" />
+          </form>
+          )
+      }
+  </div>
+)
+}
---
+++ Client/src/components/comments/EditDeleteComment.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import {addCommentArticle, getArticle} from "../../redux/actions/article.actions"
+import {useDispatch, useSelector} from "react-redux"
+
+
+const EditDeleteComment = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditDeleteComment
