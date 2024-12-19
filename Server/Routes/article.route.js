b00f2c07b16ac9bb4a4d72743965016cf97e5057
Eriuden <antoinecarrette@hotmail.fr>
2024-12-19 09:28:12 +0100

    build: adding article routes

---
+++ Server/Routes/article.route.js
@@ -0,0 +1,21 @@
+const router = require("express").Router()
+const articleController = require("../Controllers/article.controller")
+const uploadController = require("../Controllers/upload.controller")
+const multer = require("multer")
+const upload = multer()
+
+router.get("/", articleController.readArticle)
+router.get("/:id", articleController.updateArticle)
+router.get("/:id", articleController.deleteArticle)
+router.post("/", upload.single("file", articleController.createArticle))
+router.post("/upload-articlePic", upload.single("file"), uploadController.uploadArticlePic)
+router.patch("/like-article/:id", articleController.likeArticle)
+router.patch("/dislike-article/:id", articleController.dislikeArticle)
+router.patch("/unlike-article/:id", articleController.unlikeArticle)
+router.patch("/undislike-article/:id", articleController.undislikeArticle)
+
+router.patch("/comment-article/:id", articleController.commentArticle)
+router.patch("/edit-comment-article/:id", articleController.editCommentArticle)
+router.patch("/delete-comment-article/:id", articleController.deleteCommentArticle)
+
+module.exports = router
