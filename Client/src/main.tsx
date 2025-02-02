fdbd5616ebd06e7b847df2aed5e80de8fd0ae79f
Eriuden <antoinecarrette@hotmail.fr>
2025-02-02 14:10:15 +0100

    fix: Main 02/02/2025

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { reducers } from './Redux/Reducers/indexReducer.js'
 import './index.css'
 import App from './App'
 
+const store = configureStore({reducer:reducers})
 createRoot(document.getElementById('root')!).render(
+  
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
