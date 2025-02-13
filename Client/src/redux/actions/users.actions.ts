b1cce47f50a9d82a6ac6a779e984ea053194d059
Eriuden <antoinecarrette@hotmail.fr>
2025-02-13 19:46:17 +0100

    fix: users action 2 13/02/2025

--- Client/src/redux/actions/users.actions.ts
+++ Client/src/redux/actions/users.actions.ts
@@ -2,13 +2,11 @@ import axios from "axios";
 
 export const GET_USERS = "GET_USERS"
 
-export const getUsers = () => {
-    return (dispatch: any) => {
+export const getUsers = (dispatch:any) => {
         return axios
             .get(`${process.env.REACT_APP_API_URL}api/user`)
             .then((res)=> {
                 dispatch({type: GET_USERS, payload: res.data})
             })
             .catch((err) => window.alert(err))
-    }
 }
